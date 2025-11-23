4ca5d933db81707411b67276c75d8ba8bce82021
Harsh M <52977247+harshmriduhash@users.noreply.github.com>
2025-11-23 22:35:34 +0530

    Update deps.py

--- backend/app/api/deps.py
+++ backend/app/api/deps.py
@@ -42,7 +42,6 @@ def get_current_user(session: SessionDep, token: TokenDep) -> User:
         raise HTTPException(status_code=400, detail="Inactive user")
     return user
 
-
 CurrentUser = Annotated[User, Depends(get_current_user)]
 
 
