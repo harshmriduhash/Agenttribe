79f1888a91d6f8f8a29ddc96f3b352ad0c93771c
Harsh M <52977247+harshmriduhash@users.noreply.github.com>
2025-11-23 22:35:45 +0530

    Update deps.py

--- backend/app/api/deps.py
+++ backend/app/api/deps.py
@@ -44,7 +44,6 @@ def get_current_user(session: SessionDep, token: TokenDep) -> User:
 
 CurrentUser = Annotated[User, Depends(get_current_user)]
 
-
 def get_current_active_superuser(current_user: CurrentUser) -> User:
     if not current_user.is_superuser:
         raise HTTPException(
